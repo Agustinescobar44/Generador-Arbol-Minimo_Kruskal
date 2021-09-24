d3bfd69c0213c829b63ac105cd7d8faa72305bea
agust <agust@192.168.0.14>
2021-09-24 17:46:13 -0300

    agregado test a aristas

---
+++ tp2_prograTres/src/tp2_prograTres/AristaTest.java
@@ -0,0 +1,33 @@
+package tp2_prograTres;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AristaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCompareAMenor() {
+		Arista a = new Arista(0, 1, 120);
+		Arista b = new Arista(0, 1, 2000);
+		assertEquals(-1, a.compareTo(b));
+	}
+	@Test
+	public void testCompareAMayor() {
+		Arista a = new Arista(0, 1, 120);
+		Arista b = new Arista(0, 1, 2);
+		assertEquals(1, a.compareTo(b));
+	}
+	@Test
+	public void testCompareAigual() {
+		Arista a = new Arista(0, 1, 120);
+		Arista b = new Arista(0, 1, 120);
+		assertEquals(0, a.compareTo(b));
+	}
+
+}
