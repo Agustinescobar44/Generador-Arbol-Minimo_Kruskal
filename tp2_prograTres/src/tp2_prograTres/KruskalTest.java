d7ff3bf0ff2746dbe7aa052c4d1efcb0d7a8feb5
agust <agust@192.168.0.14>
2021-09-24 17:34:47 -0300

    arreglados test a nueva implementacion

--- tp2_prograTres/src/tp2_prograTres/KruskalTest.java
+++ tp2_prograTres/src/tp2_prograTres/KruskalTest.java
@@ -7,25 +7,44 @@ import org.junit.Test;
 
 public class KruskalTest {
 
-	Kruskal kruskal = new Kruskal(9);
+	GrafoConPesos g;
 	@Before
 	public void setUp() throws Exception {
 	}
 
 	@Test
 	public void testDameMinimaNoMarcada() {
+		g = new GrafoConPesos(5);
+		g.setArista(0, 1, 150);
+		g.setArista(1, 2, 200);
+		g.setArista(2, 4, 300);
+		g.setArista(4, 3, 75);
+		g.setArista(3, 1,500);
+		g.setArista(4, 0,100);
 		
-		kruskal.g.setArista(2, 3, 350);
-		kruskal.g.setArista(0, 1, 250);
-		kruskal.g.setArista(1, 2, 120);
+		GrafoConPesos arbol = new GrafoConPesos(5);
 		
-		kruskal.agregarArista(new Arista(1,2,120));
-		Arista minima = kruskal.dameMinimaNoMarcada();
-		assertEquals(new Arista(0, 1, 250), minima);
+		arbol.setArista(4, 3, 75);
+		arbol.setArista(4, 0, 100);
 		
-		kruskal.agregarArista(new Arista(0,1,250));
-		minima = kruskal.dameMinimaNoMarcada();
-		assertEquals(new Arista(2, 3, 350), minima);
+		assertEquals(new Arista(0, 1, 150), Kruskal.dameMinimaNoConexa(g,arbol));
+		
 	}
 
+	@Test 
+	public void testHaceCircuitoFalso(){
+		g = new GrafoConPesos(3);
+		g.setArista(0, 1, 120);
+		
+		assertFalse(Kruskal.haceCircuito(g, 0, 2));
+	}
+	@Test 
+	public void testHaceCircuitoVerdadero(){
+		g = new GrafoConPesos(3);
+		g.setArista(0, 1, 120);
+		g.setArista(2, 1, 100);
+		
+		assertTrue(Kruskal.haceCircuito(g, 0, 2));
+	}
+	
 }
