58a2ff6ad4191fc459a01c8d6c4a037eb2660069
agust <agust@192.168.0.14>
2021-09-24 17:34:15 -0300

    Agregado metodo haceCircuito(grafo , int origen , int destino), cambiado dameMinimaNoMarcada() por dameMinimaNoConexa(), eliminado constructor de kruskal e implementado metodo kruskal

--- tp2_prograTres/src/tp2_prograTres/Kruskal.java
+++ tp2_prograTres/src/tp2_prograTres/Kruskal.java
@@ -1,47 +1,45 @@
 package tp2_prograTres;
 
+import java.util.Set;
 
 public class Kruskal {
 
-	GrafoConPesos g; 
-	GrafoConPesos arbolGenerado ;
-	int[] unionFind;
+	static GrafoConPesos arbolGenerado ;
 	
-	public Kruskal(int vertices) {
-		g = new GrafoConPesos(vertices);
+	public static GrafoConPesos kruskal(GrafoConPesos g) {
 		
-		arbolGenerado = new GrafoConPesos(vertices);
+		arbolGenerado = new GrafoConPesos(g.vertices());
 		
-		unionFind = new int[vertices];
-		for (int i = 0; i < vertices; i++) {
-			unionFind[i] = i;
+		int i = 0;
+		while (i<g.vertices()-1) {
+			Arista arista = dameMinimaNoConexa(g , arbolGenerado); //arista a agregar en el arbol
 			
+			arbolGenerado.setArista(arista.getA(), arista.getB(),arista.getPeso());
+			System.out.println(arbolGenerado.getAristas());
+			i+=1; //manejo de indice
 		}
+		
+		return arbolGenerado;
 	}
 
-	public Arista dameMinimaNoMarcada() {
-		Arista temp = g.getAristas().get(0);
-		for (Arista arista : g.getAristas()) {
-			
-			if(arista.compareTo(temp)<0 && !arbolGenerado.existeArista(arista.getA(),arista.getB())) {
+	//agrega la arista a el arbol generado
+	private void agregarArista(Arista arista) {
+		arbolGenerado.setArista(arista.getA(), arista.getB(), arista.getPeso());;
+	}
+	
+	
+	public static Arista dameMinimaNoConexa(GrafoConPesos grafo , GrafoConPesos arbol) {
+		Arista temp = grafo.dameAristaMaxima(); //inicio con la primer arista para comparar
+		for (Arista arista : grafo.getAristas()) { //recorro el grafo
+			if(arista.compareTo(temp)<0 && !haceCircuito(arbol, arista.getA(), arista.getB())) {
 				temp = arista;
 			}
 		}
 		return temp;
-		
-	}
-
-	public void agregarArista(Arista arista) {
-		arbolGenerado.setArista(arista.getA(), arista.getB(), arista.getPeso());;
 	}
 	
-	public void setArista(int a, int b, int peso) {
-		g.setArista(a, b, peso);
-		
+	public static boolean haceCircuito(GrafoConPesos g , int origen ,int destino) {
+		Set<Integer> alcanzables =  BFS.alcanzables(g, origen);
+		return alcanzables.contains(destino);
 	}
-	
-	
-	
-	
-	
 }
