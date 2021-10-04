896aefbd1b0ffacda31017dadf802c54cdec260c
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-04 20:00:37 -0300

    Primer acercamiento a medicion , no saca el promedio

--- tp2_prograTres/src/metodos/Principal.java
+++ tp2_prograTres/src/metodos/Principal.java
@@ -1,5 +1,8 @@
 package metodos;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import estructurasDeDatos.GrafoConPesos;
 
 public class Principal {
@@ -15,16 +18,15 @@ public class Principal {
 		return grafoGenerado;
 	}
 	
-	public static long promedioKruskal(String tipoKruskal, int veces, int vertices, int aristas) {
-		GrafoConPesos a=generarGrafoConexo(vertices,aristas);
+	public static long promedioKruskalEnGrafo(String tipoKruskal, int veces, GrafoConPesos a) {
 		long inicio = System.currentTimeMillis();
-		
-		if (tipoKruskal.equals("BFS")) {
+		tipoKruskal.toLowerCase();
+		if (tipoKruskal.equals("bfs")) {
 			for(int i=0; i<veces ; i++) {
 				KruskalBFS.kruskal(a);
 			}
 		}
-		else if(tipoKruskal.equals("UnionFind")) {
+		else if(tipoKruskal.equals("unionfind")) {
 			for(int i=0; i<veces ; i++) {
 				KruskalUnionFind.Kruskal(a);
 			}
@@ -34,30 +36,95 @@ public class Principal {
 		}
 		
 		long fin = System.currentTimeMillis();
-		long total=(fin-inicio)/veces;
+		long total=(fin-inicio);
+		
+		return total;
+	}
+	public static long promedioKruskalSetDeGrafos(String tipoKruskal,Set<GrafoConPesos> grafos) {
+		long inicio = System.currentTimeMillis();
+		tipoKruskal.toLowerCase();
+		if (tipoKruskal.equals("bfs")) {
+			for (GrafoConPesos grafoConPesos : grafos) {
+				KruskalBFS.kruskal(grafoConPesos);
+			}
+		}
+		else if(tipoKruskal.equals("unionfind")) {
+			for (GrafoConPesos grafoConPesos : grafos) {
+				KruskalUnionFind.Kruskal(grafoConPesos);
+			}
+		}
+		else {
+			throw new RuntimeException("El tipo de implementacion de kruskal no es correcto");
+		}
+		
+		long fin = System.currentTimeMillis();
+		long total=(fin-inicio);
 		
 		return total;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- 
-		//tomando tiempo de Kruskal con BFS (grafos de 10 vertices, 9 aristas)
-		System.out.println("tiempo promedio del metodo BFS (grafos de 10 vertices, 15 aristas)= " + promedioKruskal("BFS",100,10,15));;
-			
-			
-		//tomando tiempo de Kruskal con BFS (grafos de 50 vertices, 69 aristas)
-		System.out.println("tiempo promedio del metodo BFS (grafos de 50 vertices, 49 aristas)= " + promedioKruskal("BFS",100,50,69));
-			
-			
-		//tomando tiempo de Kruskal con UnionFind (grafos de 10 vertices, 9 aristas)
-
-		System.out.println("tiempo promedio del algoritmo Kruskal con UnionFind (grafos de 10 vertices, 15 aristas)= " + promedioKruskal("UnionFind",100,10,15));
-			
-		//tomando tiempo de Kruskal con UnionFind (grafos de 50 vertices, 69 aristas)
-		System.out.println("tiempo promedio del algoritmo Kruskal con UnionFind (grafos de 50 vertices, 69 aristas)= " + promedioKruskal("UnionFind",100,50,69));
-			 
-		KruskalUnionFind.Kruskal(generarGrafoConexo(10,15));
-		System.out.println(KruskalUnionFind.Kruskal(generarGrafoConexo(10,15)).getAristas());
+		//==============Variables==============
+		StringBuilder ret = new StringBuilder();
+		int verticesG = 50; 
+		int cantidadDeEjecuciones=100;
+		
+		int verticesMuchos = 50;
+		int cantidadGrafos = 200;
+		
+		//==============Graficar==============
+		ret.append("Mediciones multiples sobre una instancia\n---------------------\n");
+		medirEnG(verticesG,cantidadDeEjecuciones,ret);
+		verticesG = 100;
+		medirEnG(verticesG,cantidadDeEjecuciones,ret);
+		verticesG = 150;
+		medirEnG(verticesG,cantidadDeEjecuciones,ret);
+		verticesG = 200;
+		medirEnG(verticesG,cantidadDeEjecuciones,ret);
+		ret.append("===============================================\n");
+		ret.append("Mediciones unicas sobre "+cantidadGrafos+" instancias\n---------------------\n");
+		medirEnMuchos(cantidadGrafos,verticesMuchos,ret);
+		
+		System.out.println(ret);
 	}
+	
+		private static void medirEnG(int vertices,int veces,StringBuilder ret) {
+		//==============Generacion del Grafo==============
+		int verticesG = vertices;
+		int aristasG = verticesG+(verticesG/2);
+		int cantidadDeEjecuciones=veces;
+		GrafoConPesos g= generarGrafoConexo(verticesG, aristasG);
+		
+		//==============Medicion de tiempo==============
+		long tiempoBFSEnG = promedioKruskalEnGrafo("bfs", cantidadDeEjecuciones, g);
+		long tiempoUnionFindEnG = promedioKruskalEnGrafo("unionfind", cantidadDeEjecuciones, g);
+		
+		//==============Graficar==============
+		ret.append("Cantidad de vertices en G= " + verticesG+" / ");
+		ret.append("Cantidad de aristas en G= " + aristasG+" / ");
+		ret.append("Cantidad de ejecuciones en G= " + cantidadDeEjecuciones+"\n");
+		ret.append("Tiempo promedio de kruskal con BFS en G= ");
+		ret.append(tiempoBFSEnG+"\n");
+		ret.append("Tiempo promedio de kruskal con UnionFind en G= ");		  
+		ret.append(tiempoUnionFindEnG+"\n*****************\n");
+	}
+		private static void medirEnMuchos(int cantidadGrafos, int vertices,StringBuilder ret) {
+			int aristas=vertices+(vertices/2);
+			//==============Generacion de Grafos==============
+			Set<GrafoConPesos> grafos = new HashSet<GrafoConPesos>();
+			for(int i=0; i<cantidadGrafos ; i++) {
+				grafos.add(generarGrafoConexo(vertices, aristas));
+			}
+			//==============Medicion de Tiempos==============
+			long tiemposBFSEnMuchos = promedioKruskalSetDeGrafos("bfs", grafos);
+			long tiemposUnionFindEnMuchos = promedioKruskalSetDeGrafos("bfs", grafos);
+			
+			//==============Graficar==============
+			ret.append("Cada instancia tiene "+vertices+ " vertices y ");
+			ret.append(aristas+ " aristas\n");
+			ret.append("Medicion de Kruskal con BFS= ");
+			ret.append(tiemposBFSEnMuchos+"\n");
+			ret.append("Medicion de Kruskal con UnionFind= ");
+			ret.append(tiemposUnionFindEnMuchos);
+		}
 }
