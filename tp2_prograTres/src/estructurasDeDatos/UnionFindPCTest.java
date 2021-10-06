1ac023ff884842c0dc74a475231d25cf8fc4a242
elizabethmg1 <with_a_kiss@hotmail.com>
2021-10-05 23:08:52 -0300

    test UnionFindPC terminado

--- tp2_prograTres/src/estructurasDeDatos/UnionFindPCTest.java
+++ tp2_prograTres/src/estructurasDeDatos/UnionFindPCTest.java
@@ -15,16 +15,16 @@ public class UnionFindPCTest {
 
 	@Test
 	public void testBuscarRaiz() {
-		unionFind.unir(0, 1);
+		unionFind.unir(1, 0);
 		unionFind.unir(3, 1);
 		unionFind.unir(2, 1);
-		unionFind.unir(0, 4);
+		unionFind.unir(4, 0);
 		int[] esperados= {0,0,0,0};
 		int[] obtenidos= new int[4];
-		obtenidos[0]=0;
-		obtenidos[1]=0;
-		obtenidos[2]=0;
-		obtenidos[3]=0;
+		obtenidos[0]=unionFind.buscarRaiz(0);
+		obtenidos[1]=unionFind.buscarRaiz(1);
+		obtenidos[2]=unionFind.buscarRaiz(2);
+		obtenidos[3]=unionFind.buscarRaiz(3);
 		assertArrayEquals(esperados, obtenidos);
  	}
 
