1a9a8d4121a1a73d3a4e4e942deeba874c7b8473
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-06 23:13:38 -0300

    mejorada deteccion de grafos no conexos

--- tp2_prograTres/src/metodos/KruskalUnionFind.java
+++ tp2_prograTres/src/metodos/KruskalUnionFind.java
@@ -19,23 +19,20 @@ public class KruskalUnionFind  {
 		
 		while (i<verticesDelGrafo-1) {
 			AristaConPeso arista = dameMinimaNoConexa(g,unionFind,AGM); //arista a agregar en el arbol
-			
-			AGM.agregarAristaConPeso(arista.getA(), arista.getB(),arista.getPeso());//agrego la arista minima que no hace circuito al arbol
-			unionFind.unir(arista.getA(), arista.getB());
-			
+			if(arista!= null) {
+				AGM.agregarAristaConPeso(arista.getA(), arista.getB(),arista.getPeso());//agrego la arista minima que no hace circuito al arbol
+				unionFind.unir(arista.getA(), arista.getB());
+			}else
+				throw new RuntimeException("El grafo no es conexo");
 			i+=1; //manejo de indice
 		}
 		
-		if(!unionFind.esConexo()) {
-//			throw new RuntimeException("El grafo tiene que ser conexo");
-			System.out.println("El grafo no es conexo!");
-		}
 		return AGM;
 			
 		}
 
 	public static AristaConPeso dameMinimaNoConexa(GrafoConPesos g,UnionFind unionFind,GrafoConPesos AGM) {
-		AristaConPeso temp = g.getMaxima();
+		AristaConPeso temp = null;
 		for (AristaConPeso arista : g.getAristas()) {
 			
 			if(arista.compareTo(temp)<0) {
