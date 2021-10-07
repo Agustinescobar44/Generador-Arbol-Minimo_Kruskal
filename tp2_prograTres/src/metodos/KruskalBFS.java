84ac76d34d16e76922e61ec3027e632175a0fdc4
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-06 23:03:53 -0300

    mejorado dameMinimaNoConexa y complejidad

--- tp2_prograTres/src/metodos/KruskalBFS.java
+++ tp2_prograTres/src/metodos/KruskalBFS.java
@@ -13,10 +13,6 @@ public class KruskalBFS {
 	// este metodo usa el algoritmo de kruskal con BFS para devolver un arbol de un grafo dado
 	public static GrafoConPesos kruskal(GrafoConPesos g) {
 		
-		if(!BFS.esConexo(g)) {
-			throw new IllegalArgumentException("El grafo tiene que ser conexo");
-		}
-		
 		if(g.vertices() == 0) {
 			return g;
 		}
@@ -31,8 +27,9 @@ public class KruskalBFS {
 		while (i<verticesDelGrafo-1) {
 			AristaConPeso arista = dameMinimaNoConexa(aristasGrafo , arbolGenerado); //arista a agregar en el arbol
 			if(arista!=null)
-			arbolGenerado.agregarAristaConPeso(arista.getA(), arista.getB(),arista.getPeso());//agrego la arista minima que no hace circuito al arbol
-			
+				arbolGenerado.agregarAristaConPeso(arista.getA(), arista.getB(),arista.getPeso());//agrego la arista minima que no hace circuito al arbol
+			else //si dameMinimaNoConexa devuelve null significa que el grafo no es conexo
+				throw new IllegalArgumentException("El grafo tiene que ser conexo");
 			i+=1; //manejo de indice
 		}
 		
@@ -42,18 +39,13 @@ public class KruskalBFS {
 	
 	//recorro el grafo y elijo la arista minima que no hace circuito en el arbol
 	public static AristaConPeso dameMinimaNoConexa(Set<AristaConPeso> aristasGrafo , GrafoConPesos arbol) {
-		AristaConPeso temp = null; //inicio con la primer arista para comparar
-		
+		AristaConPeso temp = null;
 		for (AristaConPeso arista : aristasGrafo) { //recorro el grafo
-			if(temp == null) {
-				temp = arista;
-			}
-			//si la arista es menor a la primera de las aristas del grafo y no hace circuito con otra arista del arbol, la elijo
-			else if(arista.compareTo(temp)<0 && 
-					!haceCircuito(arbol, arista.getA(), arista.getB())) {
+			if(arista.compareTo(temp)<0 && 
+					!haceCircuito(arbol, arista.getA(), arista.getB())) 
 				
 				temp = arista;
-			}
+			
 		}
 		aristasGrafo.remove(temp); //remuevo la arista del set de aristas del grafo
 		return temp;
