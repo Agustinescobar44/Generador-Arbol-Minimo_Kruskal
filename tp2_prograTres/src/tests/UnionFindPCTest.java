b1be1e5afee646cf74e0ff063705f98c43a28eb4
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-06 23:17:46 -0300

    arreglado lugar del test

--- tp2_prograTres/src/tests/UnionFindPCTest.java
+++ tp2_prograTres/src/tests/UnionFindPCTest.java
@@ -1,9 +1,11 @@
-package estructurasDeDatos;
+package tests;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import estructurasDeDatos.UnionFindPC;
 
 public class UnionFindPCTest {
 	
