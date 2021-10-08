b859577eacbd8cc57e50eb6d6d99ac2d9ef34154
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-08 12:05:36 -0300

    Revert "Mejorada complejidad cambiando el set de aristasconpeso a un map"
    This reverts commit de9c0e009236c525eb296d9146302c37e69ed909.

--- tp2_prograTres/src/estructurasDeDatos/GrafoConPesos.java
+++ tp2_prograTres/src/estructurasDeDatos/GrafoConPesos.java
@@ -1,22 +1,22 @@
 package estructurasDeDatos;
 
-import java.util.HashMap;
-import java.util.Set;
+import java.util.Collections;
+import java.util.HashSet;
 
 
 public class GrafoConPesos extends Grafo {
 
-	private HashMap<AristaConPeso,Double> aristas;
+	private HashSet<AristaConPeso> aristas;
 	public GrafoConPesos(int i) {
 		super(i);
-		aristas = new HashMap<>();
+		aristas = new HashSet<AristaConPeso>();
 	}
 	
 	public void agregarAristaConPeso(int a , int b , double peso) {
 		AristaConPeso arista = new AristaConPeso(a, b, peso);
 		if(!existeArista(a, b)) {
 			agregarArista(a, b);
-			aristas.put(arista, peso);
+			aristas.add(arista);
 		}
 		else {
 			throw new RuntimeException("La arista ya existe");
@@ -27,16 +27,23 @@ public class GrafoConPesos extends Grafo {
 		if(!existeArista(a, b)) {
 			throw new RuntimeException("La arista no existe en el grafo");
 		}
-		AristaConPeso ar = new AristaConPeso(a, b, 0);
-		
-		return aristas.get(ar);
+
+		for (AristaConPeso arista : aristas) {
+				if((arista.getA() == a && arista.getB() == b)||
+						(arista.getA() == b && arista.getB() == a)) {
+						return arista.getPeso();
+				}
+		}
+		return 0;
 	}
 	
 	
-	public Set<AristaConPeso> getAristas(){
-		return aristas.keySet();
+	public HashSet<AristaConPeso> getAristas(){
+		return (HashSet<AristaConPeso>) aristas.clone();
 	}
 
-
+	public AristaConPeso getMaxima() {
+		return Collections.max(aristas);
+	}
 	
 }
